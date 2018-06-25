1020f3e808b2aa95a523a1ca7ab5cbe2b62bd0fe
Nate Mara <nate.mara@kroger.com>
2018-06-25 12:07:42 -0400

    Update CreateComponentOptions to include JSON field tags

--- component.go
+++ component.go
@@ -15,29 +15,23 @@ type ComponentService struct {
 
 // CreateComponentOptions are passed to the ComponentService.Create function to create a new JIRA component
 type CreateComponentOptions struct {
-	Name         string
-	Description  string
-	LeadUserName string
-	AssigneeType string
-	Lead         *User
-	Assignee     *User
-	Project      string
-	ProjectID    *int
+	Name         string `json:"name,omitempty" structs:"name,omitempty"`
+	Description  string `json:"description,omitempty" structs:"description,omitempty"`
+	Lead         *User  `json:"lead,omitempty" structs:"lead,omitempty"`
+	LeadUserName string `json:"leadUserName,omitempty" structs:"leadUserName,omitempty"`
+	AssigneeType string `json:"assigneeType,omitempty" structs:"assigneeType,omitempty"`
+	Assignee     *User  `json:"assignee,omitempty" structs:"assignee,omitempty"`
+	Project      string `json:"project,omitempty" structs:"project,omitempty"`
+	ProjectID    int    `json:"projectId,omitempty" structs:"projectId,omitempty"`
 }
 
 // FullComponent is a JIRA component with all details filled in
 type FullComponent struct {
-	Name                string `json:"name,omitempty" structs:"name,omitempty"`
-	Description         string `json:"description,omitempty" structs:"description,omitempty"`
-	Lead                *User  `json:"lead,omitempty" structs:"lead,omitempty"`
-	Assignee            *User  `json:"assignee,omitempty" structs:"assignee,omitempty"`
-	LeadUserName        string `json:"leadUserName,omitempty" structs:"leadUserName,omitempty"`
-	AssigneeType        string `json:"assigneeType,omitempty" structs:"assigneeType,omitempty"`
 	RealAssigneeType    string `json:"realAssigneeType,omitempty" structs:"realAssigneeType,omitempty"`
 	RealAssignee        *User  `json:"realAssignee,omitempty" structs:"realAssignee,omitempty"`
 	IsAssigneeTypeValid bool   `json:"isAssigneeTypeValid,omitempty" structs:"isAssigneeTypeValid,omitempty"`
-	Project             string `json:"project,omitempty" structs:"project,omitempty"`
-	ProjectID           int    `json:"projectId,omitempty" structs:"projectId,omitempty"`
+
+	CreateComponentOptions
 }
 
 // Create creates a new JIRA component based on the given options.
