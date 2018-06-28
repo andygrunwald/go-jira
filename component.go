77ea07b2ceb794d3f16a2517cb93109109289354
Nate Mara <nate.mara@kroger.com>
2018-06-28 12:31:23 -0400

    Simplify deserialization logic

--- component.go
+++ component.go
@@ -1,11 +1,5 @@
 package jira
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-)
-
 // ComponentService handles components for the JIRA instance / API.
 //
 // JIRA API docs: https://docs.atlassian.com/software/jira/docs/api/REST/7.10.1/#api/2/component
@@ -32,21 +26,9 @@ func (s *ComponentService) Create(options *CreateComponentOptions) (*ProjectComp
 	if err != nil {
 		return nil, nil, err
 	}
-	resp, err := s.client.Do(req, nil)
-	if err != nil {
-		// incase of error return the resp for further inspection
-		return nil, resp, err
-	}
 
 	component := new(ProjectComponent)
-	defer resp.Body.Close()
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, resp, fmt.Errorf("Could not read the returned data")
-	}
-	err = json.Unmarshal(data, component)
-	if err != nil {
-		return nil, resp, fmt.Errorf("Could not unmarshall the data into struct")
-	}
-	return component, resp, nil
+	resp, err := s.client.Do(req, component)
+
+	return component, resp, err
 }
