b2bcdc9f43fb4fcb9168ec3111d82fed41ea1047
Matthias Weiss <matthias.weiss@edg.ag>
2018-11-20 12:54:02 +0100

    Add PermissionSchemeService
    Expand the PermissionScheme struct of ProjectService.
    
    Add new PermissionSchemeService with functions Get and GetList.
    
    Add corresponding mocks and tests.

--- project.go
+++ project.go
@@ -74,11 +74,12 @@ type ProjectComponent struct {
 
 // PermissionScheme represents the permission scheme for the project
 type PermissionScheme struct {
-	Expand      string `json:"expand" structs:"expand,omitempty"`
-	Self        string `json:"self" structs:"self,omitempty"`
-	ID          int    `json:"id" structs:"id,omitempty"`
-	Name        string `json:"name" structs:"name,omitempty"`
-	Description string `json:"description" structs:"description,omitempty"`
+	Expand      string       `json:"expand" structs:"expand,omitempty"`
+	Self        string       `json:"self" structs:"self,omitempty"`
+	ID          int          `json:"id" structs:"id,omitempty"`
+	Name        string       `json:"name" structs:"name,omitempty"`
+	Description string       `json:"description" structs:"description,omitempty"`
+	Permissions []Permission `json:"permissions" structs:"permissions,omitempty"`
 }
 
 // GetList gets all projects form JIRA
