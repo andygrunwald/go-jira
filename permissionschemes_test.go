3083c4bc7ffd4d2c7fc16646f0e592f536365280
Matthias Weiss <matthias.weiss@edg.ag>
2019-05-09 13:13:15 +0200

    Add negative tests for PermissionService

--- permissionschemes_test.go
+++ permissionschemes_test.go
@@ -24,10 +24,37 @@ func TestPermissionSchemeService_GetList(t *testing.T) {
 
 	permissionScheme, _, err := testClient.PermissionScheme.GetList()
 	if permissionScheme == nil {
-		t.Error("Expected role list. Role list is nil")
+		t.Error("Expected permissionScheme list. PermissionScheme list is nil")
+	}
+	if len(permissionScheme.PermissionSchemes) != 2 {
+		t.Errorf("Expected %d permissionSchemes but got %d", 2, len(permissionScheme.PermissionSchemes))
 	}
 	if err != nil {
 		t.Errorf("Error given: %v", err)
+	}
+}
+
+func TestPermissionSchemeService_GetList_NoList(t *testing.T) {
+	setup()
+	defer teardown()
+	testAPIEndpoint := "/rest/api/3/permissionscheme"
+
+	raw, err := ioutil.ReadFile("./mocks/no_permissionschemes.json")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	testMux.HandleFunc(testAPIEndpoint, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		testRequestURL(t, r, testAPIEndpoint)
+		fmt.Fprintf(w, string(raw))
+	})
+
+	permissionScheme, _, err := testClient.PermissionScheme.GetList()
+	if permissionScheme != nil {
+		t.Errorf("Expected permissionScheme list has %d entries but should be nil", len(permissionScheme.PermissionSchemes))
+	}
+	if err == nil {
+		t.Errorf("No error given")
 	}
 }
 
@@ -47,10 +74,32 @@ func TestPermissionSchemeService_Get(t *testing.T) {
 
 	permissionScheme, _, err := testClient.PermissionScheme.Get(10100)
 	if permissionScheme == nil {
-		t.Errorf("Expected Role, got nil")
+		t.Errorf("Expected permissionscheme, got nil")
 	}
 	if err != nil {
 		t.Errorf("Error given: %s", err)
 	}
+}
 
+func TestPermissionSchemeService_Get_NoScheme(t *testing.T) {
+	setup()
+	defer teardown()
+	testAPIEdpoint := "/rest/api/3/permissionscheme/99999"
+	raw, err := ioutil.ReadFile("./mocks/no_permissionscheme.json")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	testMux.HandleFunc(testAPIEdpoint, func(writer http.ResponseWriter, request *http.Request) {
+		testMethod(t, request, "GET")
+		testRequestURL(t, request, testAPIEdpoint)
+		fmt.Fprintf(writer, string(raw))
+	})
+
+	permissionScheme, _, err := testClient.PermissionScheme.Get(99999)
+	if permissionScheme != nil {
+		t.Errorf("Expected nil, got permissionschme %v", permissionScheme)
+	}
+	if err == nil {
+		t.Errorf("No error given")
+	}
 }
