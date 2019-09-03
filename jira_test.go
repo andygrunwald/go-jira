a8bdfed27ff42a9bb0468b8cf192871780919def
Shaun Dunning <shaun.dunning@uservoice.com>
2019-09-03 17:53:37 -0400

    feat: add support for JWT auth with qsh needed by add-ons

--- jira_test.go
+++ jira_test.go
@@ -612,3 +612,27 @@ func TestCookieAuthTransport_SessionObject_DoesNotExist(t *testing.T) {
 	req, _ := basicAuthClient.NewRequest("GET", ".", nil)
 	basicAuthClient.Do(req, nil)
 }
+
+func TestJWTAuthTransport_HeaderContainsJWT(t *testing.T) {
+	setup()
+	defer teardown()
+
+	sharedSecret := []byte("ssshh,it's a secret")
+	issuer := "add-on.key"
+
+	jwtTransport := &JWTAuthTransport{
+		Secret: sharedSecret,
+		Issuer: issuer,
+	}
+
+	testMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// look for the presence of the JWT in the header
+		val := r.Header.Get("Authorization")
+		if !strings.Contains(val, "JWT ") {
+			t.Errorf("request does not contain JWT in the Auth header")
+		}
+	})
+
+	jwtClient, _ := NewClient(jwtTransport.Client(), testServer.URL)
+	jwtClient.Issue.Get("TEST-1", nil)
+}
