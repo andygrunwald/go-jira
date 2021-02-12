5ba132b63eb4a9b213926f0c61d7bb5511598f6a
manuelbcd <manuelbcd@gmail.com>
2021-02-12 12:38:56 +0000

    Added example creating issue with custom fields

---
+++ examples/customfields/create-with-customfields/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+	"syscall"
+
+	"github.com/trivago/tgo/tcontainer"
+	jira "github.com/andygrunwald/go-jira"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+func printStruct(i interface{}) string {
+	s, _ := json.MarshalIndent(i, "", "\t")
+	return string(s)
+}
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Jira URL: ")
+	jiraURL, _ := r.ReadString('\n')
+
+	fmt.Print("Jira Username: ")
+	username, _ := r.ReadString('\n')
+
+	fmt.Print("Jira Password: ")
+	bytePassword, _ := terminal.ReadPassword(int(syscall.Stdin))
+	password := string(bytePassword)
+
+	fmt.Print("Custom field name (i.e. customfield_10220): ")
+	customfieldname, _ := r.ReadString('\n')
+
+	fmt.Print("Custom field value: ")
+	customfieldvalue, _ := r.ReadString('\n')
+
+	tp := jira.BasicAuthTransport{
+		Username: strings.TrimSpace(username),
+		Password: strings.TrimSpace(password),
+	}
+
+	client, err := jira.NewClient(tp.Client(), strings.TrimSpace(jiraURL))
+	if err != nil {
+		fmt.Printf("\nerror: %v\n", err)
+		return
+	}
+
+	unknowns := tcontainer.NewMarshalMap()
+	unknowns[customfieldname] = customfieldvalue
+
+	i := jira.Issue{
+		Fields: &jira.IssueFields{
+			Assignee: &jira.User{
+				Name: "myuser",
+			},
+			Reporter: &jira.User{
+				Name: "youruser",
+			},
+			Description: "Test Issue",
+			Type: jira.IssueType{
+				Name: "Bug",
+			},
+			Project: jira.Project{
+				Key: "PROJ1",
+			},
+			Summary: "Just a demo issue",
+			Unknowns: unknowns,
+		},
+	}
+
+	issue, _, err := client.Issue.Create(&i)
+	if err != nil {
+		panic(err)
+	}
+
+	// Please not that modern Jira versions (>8.x) does not include Summary field in the response
+	// fmt.Printf("%s: %+v\n", issue.Key, issue.Fields.Summary)
+
+	// Jira versions (>8.x)
+	fmt.Printf(printStruct(issue))
+}
+
