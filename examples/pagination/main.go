97404d7e0caa6e4d8d5e7732b078ff507ba15c01
Jay Patel <jay.patel.2@team.telstra.com>
2021-03-03 10:00:55 +1100

    update as requested

--- examples/pagination/main.go
+++ examples/pagination/main.go
@@ -6,13 +6,16 @@ import (
 	jira "github.com/andygrunwald/go-jira"
 )
 
-//GetAllIssues takes a jira client and returns all issues for given JQL
+// GetAllIssues will implement pagination of api and get all the issues.
+// Jira API has limitation as to maxResults it can return at one time.
+// You may have usecase where you need to get all the issues according to jql
+// This is where this example comes in.
 func GetAllIssues(client *jira.Client, searchString string) ([]jira.Issue, error) {
 	last := 0
-	var issues []jira.Issue = nil
+	var issues []jira.Issue
 	for {
 		opt := &jira.SearchOptions{
-			MaxResults: 100,
+			MaxResults: 1000, // Max results can go upto 1000
 			StartAt:    last,
 		}
 
@@ -21,25 +24,25 @@ func GetAllIssues(client *jira.Client, searchString string) ([]jira.Issue, error
 			return nil, err
 		}
 
-		total := resp.Total
 		if issues == nil {
+			total := resp.Total
 			issues = make([]jira.Issue, 0, total)
 		}
 		issues = append(issues, chunk...)
 		last = resp.StartAt + len(chunk)
 		if last >= total {
-			break
+			return issues, nil
 		}
 	}
 	return issues, nil
 }
 
 func main() {
-	jiraClient, _ := jira.NewClient(nil, "https://issues.apache.org/jira/")
+	jiraClient, err := jira.NewClient(nil, "https://issues.apache.org/jira/")
+	if err != nil {
+		panic(err)
+	}
 
-	// Jira API has limitation as to maxResults it can return at one time.
-	// You may have usecase where you need to get all the issues according to jql
-	// This is where this example comes in.
 	jql := "project = Mesos and type = Bug and Status NOT IN (Resolved)"
 	fmt.Printf("Usecase: Running a JQL query '%s'\n", jql)
 
