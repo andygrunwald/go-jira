3aaa5ec8bf62f2cc4406532fc54756153327a463
Andy Grunwald <andygrunwald@gmail.com>
2022-08-25 17:05:30 +0300

    Splitting Jira client into OnPremise and Cloud
    Both products look like the same, but the API differs.
    We split those into their own packages to make it explicit.
    Splitting was done by
    * copying the files
    * renaming the packages
    * fixing the path to mock-test-data

---
+++ cloud/statuscategory_test.go
@@ -0,0 +1,32 @@
+package cloud
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+)
+
+func TestStatusCategoryService_GetList(t *testing.T) {
+	setup()
+	defer teardown()
+	testAPIEdpoint := "/rest/api/2/statuscategory"
+
+	raw, err := os.ReadFile("../testing/mock-data/all_statuscategories.json")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	testMux.HandleFunc(testAPIEdpoint, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		testRequestURL(t, r, testAPIEdpoint)
+		fmt.Fprint(w, string(raw))
+	})
+
+	statusCategory, _, err := testClient.StatusCategory.GetList()
+	if statusCategory == nil {
+		t.Error("Expected statusCategory list. StatusCategory list is nil")
+	}
+	if err != nil {
+		t.Errorf("Error given: %s", err)
+	}
+}
---
+++ onpremise/resolution_test.go
@@ -0,0 +1,32 @@
+package onpremise
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+)
+
+func TestResolutionService_GetList(t *testing.T) {
+	setup()
+	defer teardown()
+	testAPIEdpoint := "/rest/api/2/resolution"
+
+	raw, err := os.ReadFile("../testing/mock-data/all_resolutions.json")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	testMux.HandleFunc(testAPIEdpoint, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		testRequestURL(t, r, testAPIEdpoint)
+		fmt.Fprint(w, string(raw))
+	})
+
+	resolution, _, err := testClient.Resolution.GetList()
+	if resolution == nil {
+		t.Error("Expected resolution list. Resolution list is nil")
+	}
+	if err != nil {
+		t.Errorf("Error given: %s", err)
+	}
+}
