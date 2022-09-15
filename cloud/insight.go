cc699c60c15d57a21a8a1c91e2b2f5f3ead22cff
Florian Kinder <florian.kinder@fankserver.com>
2022-09-15 15:53:23 +0200

    fix

--- cloud/insight.go
+++ cloud/insight.go
@@ -1,12 +1,5 @@
 package cloud
 
-import "errors"
-
-var ErrValidation = errors.New("the system cannot fulfill the request due to validation errors")
-var ErrUnauthorized = errors.New("client must be authenticated to access this resource")
-var ErrNotFound = errors.New("target resource do not exist")
-var ErrUnknown = errors.New("internal server error")
-
 // needs to be var, so we can change it for testing
 var insightURL = "https://api.atlassian.com"
 
