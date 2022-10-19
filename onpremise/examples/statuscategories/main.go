2782ff34287a72e165efc09fb76f23cd5f4832f9
Andy Grunwald <andygrunwald@gmail.com>
2022-10-19 11:06:07 +0200

    On premise/Status category: Add comments for usage example
    Related #577

--- onpremise/examples/statuscategories/main.go
+++ onpremise/examples/statuscategories/main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"context"
-	jira "github.com/andygrunwald/go-jira/v2/onpremise"
 	"log"
+
+	jira "github.com/andygrunwald/go-jira/v2/onpremise"
 )
 
 func main() {
+	jiraClient, err := jira.NewClient("https://issues.apache.org/jira/", nil)
+	if err != nil {
+		panic(err)
+	}
 
-	jiraClient, _ := jira.NewClient("https://issues.apache.org/jira/", nil)
-
+	// Showcase of StatusCategory.GetList:
+	// Getting all status categories
 	categories, resp, err := jiraClient.StatusCategory.GetList(context.TODO())
 	if err != nil {
 		log.Println(resp.StatusCode)
@@ -20,6 +25,8 @@ func main() {
 		log.Println(statusCategory)
 	}
 
+	// Showcase of StatusCategory.Get
+	// Getting a single status category
 	category, resp, err := jiraClient.StatusCategory.Get(context.TODO(), "1")
 	if err != nil {
 		log.Println(resp.StatusCode)
